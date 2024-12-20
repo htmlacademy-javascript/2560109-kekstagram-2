41c6e5acb7e33897c4ede307d3115c0335d3f0a0
Keks <keks@htmlacademy.ru>
2024-12-20 17:30:17 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/functions.js
+++ 3/js/functions.js
@@ -1,12 +1,12 @@
-const checkString = (string, length) => string.length <= length;
-const isPolydrome = (string) => {
+const checkString = (string = '', length) => string.length <= length;
+const isPolidrome = (string = '') => {
   let reverseString = '';
-  for (let i = string.replaceAll(' ','').length - 1; i >= 0; i--) {
-    reverseString += string.replaceAll(' ', '')[i];
+  for (let i = string.length - 1; i >= 0; i--){
+    reverseString += string.replaceAll(' ','')[i];
+    return reverseString.toLowerCase === string.toLowerCase;
   }
-  return reverseString.toLowerCase() === string.replaceAll(' ', '').toLowerCase();
 };
 const extractNumber = (string = '') => parseInt(string.toString().replace(/\D/g,''),10);
 extractNumber();
 checkString();
-isPolydrome();
+isPolidrome();
